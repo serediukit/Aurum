e8a3729089a4a2e65e859dbfa71cc2a2c73899a9
Valentyn Serediuk <serediukit@gmail.com>
2024-10-13 16:27:29 +0300

    done one more section

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,17 +1,16 @@
-// Modal Image Gallery
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-}
-
-
-function myFunction() {
+function showHideNavigation() {
     var x = document.getElementById("navHide");
     if (x.className.indexOf("aurum-show") === -1) {
         x.className += " aurum-show";
     } else {
         x.className = x.className.replace(" aurum-show", "");
     }
+}
+
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
 }
