fea86749ce5d5b1bd228d50cdb3dfe795943caff
Valentyn Serediuk <serediukit@gmail.com>
2024-10-14 23:33:36 +0300

    add gallery without photos

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -9,8 +9,6 @@ function showHideNavigation() {
 
 // Modal Image Gallery
 function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
+    document.getElementById("image-view").src = element.src;
+    document.getElementById("gallery-view").style.display = "block";
 }
