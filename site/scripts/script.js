676e4db44f9a6d81d6c62fe7ed330ec74294647b
Valentyn Serediuk <serediukit@gmail.com>
2024-10-20 14:11:54 +0300

    add phone form

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -7,8 +7,21 @@ function showHideNavigation() {
     }
 }
 
-// Modal Image Gallery
 function onClick(element) {
     document.getElementById("image-view").src = element.src;
     document.getElementById("gallery-view").style.display = "block";
 }
+
+// Очищує поле при натисканні на нього
+const phoneInput = document.getElementById('phone');
+phoneInput.addEventListener('click', () => {
+    phoneInput.value = '';
+});
+
+// Додаємо подію для очищення форми після відправки
+const form = document.getElementById('phone-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Запобігає перезавантаженню сторінки
+    form.submit(); // Відправляє форму
+    phoneInput.value = ''; // Очищає поле після відправки
+});
